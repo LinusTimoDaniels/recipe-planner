5f0a0900cacc0340e9bf8a10f6d8ca557c67cec6
LinusTimoDaniels <linustimo.daniels@gmail.com>
2025-01-20 12:49:34 +0100

    edit recipe implemented

--- src/main/java/ch/tbz/recipe/planner/controller/RecipeController.java
+++ src/main/java/ch/tbz/recipe/planner/controller/RecipeController.java
@@ -1,13 +1,16 @@
 package ch.tbz.recipe.planner.controller;
 
 import ch.tbz.recipe.planner.domain.Recipe;
+import ch.tbz.recipe.planner.entities.RecipeEntity;
 import ch.tbz.recipe.planner.mapper.RecipeEntityMapper;
 import ch.tbz.recipe.planner.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.rmi.NoSuchObjectException;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,9 +39,16 @@ public class RecipeController {
         return new ResponseEntity<>(service.getRecipeById(recipeId), HttpStatus.OK);
     }
 
-    @PostMapping(value = "/api/recipes")
+    @PostMapping(value = "/api/recipes", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Recipe> addRecipe(@RequestBody Recipe recipe) {
-        return new ResponseEntity<>(service.addRecipe(recipe), HttpStatus.OK);
+        Recipe createdRecipe = service.addRecipe(recipe);
+        return new ResponseEntity<>(createdRecipe, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/api/recipes/{recipeId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RecipeEntity> updateRecipe(@PathVariable UUID recipeId, @RequestBody Recipe recipe) throws NoSuchObjectException {
+        RecipeEntity updatedRecipe = service.updateRecipe(recipeId, recipe);
+        return new ResponseEntity<>(updatedRecipe, HttpStatus.OK);
     }
 
 }
